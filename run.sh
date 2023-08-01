7efcbe3c62846247872b100207179252670995fa
simewu <swuthier@uccs.edu>
2023-07-31 21:54:15 -0600

    Add ability to control log file path

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
-#gnome-terminal -- bash -c "./src/app/tor ; exec bash"
-now=$(date +'%m-%d-%y-%H-%M')
-./src/app/tor --ClientOnly 1 --UseEntryGuards 0 --Log "[circ]info file ~/Desktop/tor-${now}.log"
+#!/bin/bash
+
+# Check if an argument is provided; if not, default to desktop
+LOG_FILE_PATH="$1"
+if [ -z "$LOG_FILE_PATH" ]; then
+  LOG_FILE_PATH="~/Desktop/tor.log"
+fi
+
+# Run the Tor command with the provided or default log file path, including both "circ" and "net" logging
+./src/app/tor --ClientOnly 1 --UseEntryGuards 0 --Log "[circ]info file $LOG_FILE_PATH" --Log "[net]info file $LOG_FILE_PATH"
