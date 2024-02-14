de1479f9e2d789317e88ce2e1a21e18987839b70
Simeon Wuthier <simeonwuthier@gmail.com>
2024-02-13 21:31:07 -0700

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -7,8 +7,11 @@ start_time=$(date +%s)
 
 # Loop indefinitely
 while true; do
-	# Check if Tor is running
-	if ! pgrep tor > /dev/null 2>&1; then
+	# Execute the is_tor_running.sh script and capture its output
+	output=$(./is_tor_running.sh)
+	
+	# Check if the output indicates Tor is running
+	if echo "$output" | grep -q "TOR IS NOT RUNNING"; then
 		echo "Tor has been stopped."
 		break
 	fi
@@ -19,7 +22,7 @@ while true; do
 
 	# If more than 30 seconds have elapsed, send SIGKILL
 	if [ $elapsed -ge 30 ]; then
-		echo "    Sending SIGKILL..."
+		echo "	Sending SIGKILL..."
 		pkill -9 tor
 	else
 		# Send gentle stop signal
