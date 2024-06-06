bd46b1015cef524b46c8a6f6f03a52feb2ef25ad
simewu <swuthier@uccs.edu>
2024-06-06 12:37:25 -0600

    Update address renew script

--- renew_onion_address.py
+++ renew_onion_address.py
@@ -1,18 +1,21 @@
 from stem import Signal
 from stem.control import Controller
 
-def read_password_from_file(file_path="tor_controller_pwd.key"):
+def read_password_from_file(file_path='tor_controller_pwd.key'):
 	try:
 		with open(file_path, 'r') as file:
 			return file.read().strip()
 	except IOError as e:
-		print(f"Unable to read the Tor control password from {file_path}: {e}")
+		print(f'Unable to read the Tor control password from {file_path}: {e}')
 		exit(1)
 
 def renew_onion_service(controller):
 	# Create a new ephemeral onion service
+	t1 = time.time()
 	response = controller.create_ephemeral_hidden_service({80: 80}, await_publication=True)
-	print("New onion service address:", response.service_id + ".onion")
+	t2 = time.time()
+	print('That took', t2 - t1, 'seconds')
+	print('New onion service address:', response.service_id + '.onion')
 	return response.service_id
 
 def main():
@@ -22,15 +25,15 @@ def main():
 		controller.authenticate(password=control_password)
 
 		service_id = renew_onion_service(controller)
-		print("Press any key to remove the onion service and exit.")
+		print('Press any key to remove the onion service and exit.')
 		input()  # Wait for user input to proceed
 
 		# Remove the ephemeral onion service
 		controller.remove_ephemeral_hidden_service(service_id)
-		print("Removed onion service:", service_id)
+		print('Removed onion service:', service_id)
 
 		# Send NEWNYM signal to use a new circuit
 		controller.signal(Signal.NEWNYM)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	main()
